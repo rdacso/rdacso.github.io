a32d1c8da87165c0ce183a2d9553b06c3ec37c82
rdacso <rdacso@Rebeccas-MacBook-Pro.local>
2014-06-18 21:10:46 -0700

    navigation tree started

---
+++ script.js
@@ -0,0 +1,27 @@
+ $scope.displayTree =
+            [{
+            "name": "Dropdown",
+            "type_name": "Node",
+            "show": true,
+            "nodes": [{
+                "name": "Generators",
+                "group_name": "Node-1",
+                "show": true,
+                "nodes": [{
+                    "name": "Spells",
+                    "device_name": "Node-1-1",
+                    "show": true,
+                    "nodes": []
+                }, {
+                    "name": "Random Encounters",
+                    "device_name": "Node-1-2",
+                    "show": true,
+                    "nodes": []
+
+                }]
+            }, {
+                "name": "Databases",
+                "group_name": "Spells",
+                "show": true,
+                "nodes": [{
+                }]
